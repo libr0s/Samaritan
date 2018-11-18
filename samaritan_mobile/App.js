b80547eedea85612666ca197e808d637cb0f363c
Paulina <p.jedrzejewska97@gmail.com>
2018-11-18 11:24:38 +0100

    Added correct mobile project

---
+++ samaritan_mobile/App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Inputs from './inputs.js'
+
+const App = () => {
+   return (
+      <Inputs />
+   )
+}
+export default App
---
+++ samaritan_mobile/android/app/src/main/java/com/samaritan_mobile/MainActivity.java
@@ -0,0 +1,15 @@
+package com.samaritan_mobile;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "samaritan_mobile";
+    }
+}

46b1d047b6024ab4cb746244026a5fe86eb03def
Jan Librowski <janlibrowski@gmail.com>
2019-01-09 03:33:19 +0200

    upgrade profile route and add about

--- samaritan-web/src/App.jsx
+++ samaritan-web/src/App.jsx
@@ -9,92 +9,93 @@ import {BrowserRouter as Router, Redirect, Route} from 'react-router-dom';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loggedIn: localStorage.getItem('access_token') !== null,
+    state = {
+        loggedIn: localStorage.getItem('access_token') !== null,
     };
-  }
 
-  onLoginAction = (e, email, password) => {
-    fetch("/login", {
-      method: 'POST',
-      headers:{
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      })
-    })
-    .then(res => {
-        console.log(res.status);
-        if (res.status === 200)
-          return res.json();
-    })
-    .then(json => {
-      console.log(json);
-      localStorage.setItem('access_token', json.access_token);
-      this.setState({
-        loggedIn: true,
-      });
-    })
-    .catch( a => { console.log(a) });
+    onLoginAction = (e, email, password) => {
+        fetch("/login", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        })
+            .then(res => {
+                console.log(res.status);
+                if (res.status === 200)
+                    return res.json();
+            })
+            .then(json => {
+                console.log(json);
+                localStorage.setItem('access_token', json.access_token);
+                this.setState({
+                    loggedIn: true,
+                });
+            })
+            .catch(a => {
+                console.log(a)
+            });
 
-  }
+    }
 
-  onLogoutAction = (e) => {
-    const token = 'Bearer ' + localStorage.getItem('access_token');
-    const reqHeaders = new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': token,
-        'Accept': 'application/json',
-    });
-    console.log("req")
-    fetch('/logout', {
-        method: 'POST',
-        headers: reqHeaders,
-    })
-    .then( response => response.json() )
-    .then ( json => {
-      console.log(json);
-      this.setState({
-        loggedIn: false,
-      });
+    onLogoutAction = (e) => {
+        const token = 'Bearer ' + localStorage.getItem('access_token');
+        const reqHeaders = new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': token,
+            'Accept': 'application/json',
+        });
+        console.log("req")
+        fetch('/logout', {
+            method: 'POST',
+            headers: reqHeaders,
+        })
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                this.setState({
+                    loggedIn: false,
+                });
 
-    });
-  }
+            });
+        localStorage.removeItem('access_token');
+    }
 
-  render() {
-    const {loggedIn} = this.state;
-    console.log(loggedIn)
+    render() {
+        const {loggedIn} = this.state;
+        console.log(loggedIn)
 
-    return(
-      <Router>
-        <div className="App">
-        <MenuBar loggedIn={loggedIn} onLogoutAction={this.onLogoutAction} />
-        <Route
-          path="/register"
-          component={RegisterRoute}
-        />
-        <Route
-          path="/login"
-          render={() => (loggedIn ? (<Redirect to="/profile" />) : (<LoginRoute onLoginAction={this.onLoginAction} />)) }
-        />
-        <Route
-          path="/profile"
-          render={() => (loggedIn ? <ProfileRoute /> : <Redirect to="/" />)}
-        />
-        <Route
-          path="/about"
-          component={AboutRoute}
-        />
+        return (
+            <Router>
+                <div className="App">
+                    <MenuBar loggedIn={loggedIn} onLogoutAction={this.onLogoutAction}/>
+                    <Route
+                        path="/register"
+                        component={RegisterRoute}
+                    />
+                    <Route
+                        path="/login"
+                        render={() => (loggedIn ? (<Redirect to="/profile"/>) : (
+                            <LoginRoute onLoginAction={this.onLoginAction}/>))}
+                    />
+                    <Route
+                        path="/profile"
+                        render={() => (loggedIn ? <ProfileRoute/> : <Redirect to="/login"/>)}
+                    />
+                    <Route
+                        path="/about"
+                        component={AboutRoute}
+                    />
 
-        </div>
-      </Router>
-    );
-  }
+                </div>
+            </Router>
+        );
+    }
 }
+
 export default App;
--- samaritan-web/src/components/AboutRoute.jsx
+++ samaritan-web/src/components/AboutRoute.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 
 import { Paper } from '@material-ui/core'
 import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from "@material-ui/core/Typography/Typography";
+import Logo from '../assets/img/logo.png';
 
 const styles = theme => ({
     paper: {
-        marginTop: theme.spacing.unit * 10,
+        marginTop: theme.spacing.unit * 16,
         width: '90%',
         marginRight: 'auto',
         marginLeft: 'auto',
-        height: 1600,
+        padding: theme.spacing.unit * 6,
+        textAlign: 'justify',
     },
-    lcol: {
-
+    title: {
+        textAlign: 'center',
+        paddingBottom: theme.spacing.unit * 2,
+    },
+    logo: {
+        margin: 'auto',
+        display: 'block',
+        '@media(max-width: 605px)': {
+            display: 'none'
+        }
     }
 });
 
@@ -23,7 +34,13 @@ class AboutRoute extends React.Component {
 
         return (
             <Paper className={classes.paper}>
-                :)
+                <Typography className={classes.title} variant={"display2"}>
+                    Aim of the project
+                </Typography>
+                <img alt="logo" src={Logo} className={classes.logo}/>
+                <Typography variant={"body1"}>
+                Celem projektu jest stworzenie aplikacji zrzeszającej wolontariuszy i osoby organizujące wolontariaty. Aplikacja ma za zadanie ułatwić komunikację między potencjalnymi uczestnikami, a organizatorami, tym samym przyczyniając się do wzrostu popularności akcji charytatywnych. Aplikacja ma być dostępna poprzez stronę internetową oraz aplikację mobilną. Dostosowanie się do przedziału wiekowego osób korzystających z platformy wymaga rozdzielenia funkcji aplikacji mobilnej i internetowej. Wolontariusze to zwykle osoby młode, które prawdopodobnie częściej używają telefonów komórkowych, niż komputerów osobistych. Dlatego aplikacja mobilna ma być przeznaczona dla nich - osób zapisujących się na różnego rodzaju akcje charytatywne i społecznościowe. Przedstawiciele organizacji pozyskujących nowych, chętnych do pomocy ludzi, są zwykle etatowymi pracownikami. O wiele bardziej przystępną formą korzystania z platformy byłoby dla nich używanie komputerów osobistych - stąd strona internetowa. Wygoda korzystania z aplikacji mobilnej oraz intuicyjny proces zapisu na wydarzenia mogą zachęcić nowe osoby do wolontariatu.
+                </Typography>
             </Paper>
         );
     }
